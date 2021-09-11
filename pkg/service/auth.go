103d7f7f6307c0decdeb2116a9fd86127e962e5b
Almas <namazbaev2000@gmail.com>
2021-09-11 17:58:31 +0600

    Authentication. JWT tokens

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,23 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
 	todo "github.com/Almas016/Todo-app"
 	"github.com/Almas016/Todo-app/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "wenfjksd123otiuihjiu794"
+const (
+	salt       = "wenfjksd123otiuihjiu794"
+	signingKey = "wenfjwnfkj23nrjnfjwef"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -23,7 +34,24 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	return s.repo.CreateUser(user)
 }
 
-func (s *AuthService) generatePasswordHash(password string) string{
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
+}
+
+func (s *AuthService) generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
 
