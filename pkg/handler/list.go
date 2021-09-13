0eaee26efa8868149f9b906500aa8c88352b561f
Almas <namazbaev2000@gmail.com>
2021-09-13 21:22:09 +0600

    Creating and returning lists

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -2,29 +2,81 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
+	todo "github.com/Almas016/Todo-app"
 	"github.com/gin-gonic/gin"
 )
 
-func (h *Handler) createList(c *gin.Context){
-	id, _ := c.Get(userCtx)
+func (h *Handler) createList(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.TodoList.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 	c.JSON(http.StatusOK, map[string]interface{}{
-		"id":id,
+		"id": id,
 	})
 }
 
-func (h *Handler) getAllLists(c *gin.Context){
-	
+type getAllListsResponse struct {
+	Data []todo.TodoList `json:"data"`
 }
 
-func (h *Handler) getListById(c *gin.Context){
-	
+func (h *Handler) getAllLists(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	lists, err := h.services.TodoList.GetAll(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, getAllListsResponse{
+		Data: lists,
+	})
 }
 
-func (h *Handler) updateList(c *gin.Context){
-	
+func (h *Handler) getListById(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	list, err := h.services.TodoList.GetById(userId, id)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, list)
 }
 
-func (h *Handler) deleteList(c *gin.Context){
-	
+func (h *Handler) updateList(c *gin.Context) {
+
+}
+
+func (h *Handler) deleteList(c *gin.Context) {
+
 }
